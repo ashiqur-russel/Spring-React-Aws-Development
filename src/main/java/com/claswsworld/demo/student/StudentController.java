7dc9e02cc55060fb1c5fca3ae24c41020ef6ded1
ashiqurras <ashiqurrussel2@gmail.com>
2022-01-02 20:02:40 +0100

    Created endpoint to retirive student as JSON format

---
+++ src/main/java/com/claswsworld/demo/student/StudentController.java
@@ -0,0 +1,19 @@
+package com.claswsworld.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+@RestController
+@RequestMapping(path = "/students")
+public class StudentController {
+    @GetMapping
+    public List<Student> getAllStudent(){
+        return  List.of(
+                new Student(UUID.randomUUID(),"james","Bond","james.bond@gmail.com", Student.Gender.MALE),
+                new Student(UUID.randomUUID(),"Elisa","Tamara","elisa.tamara@gmail.com", Student.Gender.FEMALE)
+        );
+    }
+}
